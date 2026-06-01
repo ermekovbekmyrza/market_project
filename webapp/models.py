f1ce8073f131a8e24a42f3fb0f7b696e713b90d3
Ermekov Bekmyrza <ermekov.top@gmail.com>
2026-06-01 17:10:34 +0600

    Add Category and Product models with migration and admin

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость")
+    image = models.URLField(verbose_name="Изображение")
+
+    def __str__(self):
+        return self.name
+    
